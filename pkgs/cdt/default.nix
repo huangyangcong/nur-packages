50ffdabf0e520a4a3e1689c8175e18cce1a371a0
YangCongHuang <huangyangcong@gmail.com>
2023-08-23 19:57:19 +0800

    add gcc8Stdenv

--- pkgs/cdt/default.nix
+++ pkgs/cdt/default.nix
@@ -9,12 +9,16 @@ clangStdenv.mkDerivation rec {
   name = "cdt";
   version = "4.0.0";
   buildInputs = with pkgs; [
+    gcc8Stdenv
     llvm
     curl.dev
     gmp.dev
     openssl.dev
     libusb1.dev
     bzip2.dev
+    libxml2.dev
+    ocaml
+    opam
     (boost.override
       {
         enableShared = false;
@@ -22,6 +26,7 @@ clangStdenv.mkDerivation rec {
       })
   ];
   nativeBuildInputs = with pkgs; [ pkgconfig cmake clang git python3 ];
+  cmakeFlags = [ "-DCMAKE_C_COMPILER=gcc-8 -DCMAKE_CXX_COMPILER=g++-8" ];
 
   src = fetchgit {
     url = "https://github.com/AntelopeIO/cdt";
