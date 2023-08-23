39a3029d894b5e36c9060ed115ada8e76c324a22
YangCongHuang <huangyangcong@gmail.com>
2023-08-23 10:47:51 +0800

    init

---
+++ pkgs/cdt/default.nix
@@ -0,0 +1,31 @@
+{ clangStdenv
+, nodejs
+, fetchgit
+, pkgs
+, lib
+,
+}:
+clangStdenv.mkDerivation rec {
+  name = "cdt";
+  version = "4.0.0";
+  buildInputs = with pkgs; [
+    llvm
+    curl.dev
+    gmp.dev
+    openssl.dev
+    libusb1.dev
+    bzip2.dev
+    (boost.override
+      {
+        enableShared = false;
+        enabledStatic = true;
+      })
+  ];
+  nativeBuildInputs = with pkgs; [ pkgconfig cmake clang git python3 ];
+
+  src = fetchgit {
+    url = "https://github.com/AntelopeIO/cdt";
+    rev = "v${version}";
+    sha256 = "sha256-yZpTj3MlWYDw3Se7ZwwvNnqB4XpdPVqHWY2hK18KIIU=";
+  };
+}
