d6525cad7db151e93d5df706feb84c5b9318dfab
YangCongHuang <huangyangcong@gmail.com>
2023-09-01 09:52:04 +0800

    fixed blanc

--- pkgs/blanc/blanc_16.nix
+++ pkgs/blanc/blanc_16.nix
@@ -1,33 +1,20 @@
-{ clangStdenv
-, nodejs
+{ gcc10Stdenv
 , fetchFromGitHub
 , pkgs
 , lib
 , llvmPackages
 ,
 }:
-clangStdenv.mkDerivation rec {
+gcc10Stdenv.mkDerivation rec {
   name = "blanc";
   version = "16.0.0";
   buildInputs = with pkgs; [
     llvmPackages_16.clang-unwrapped
     llvmPackages_16.llvm
     llvmPackages_16.lld
-    curl.dev
-    gmp.dev
-    openssl.dev
-    libusb1.dev
-    bzip2.dev
-    libxml2.dev
-    ocaml
-    opam
-    (boost.override
-      {
-        enableShared = false;
-        enabledStatic = true;
-      })
+    gcc10
   ];
-  nativeBuildInputs = with pkgs; [ pkgconfig gcc13Stdenv cmake git wabt python3 ];
+  nativeBuildInputs = with pkgs; [ pkgconfig cmake git wabt python3 ];
   src = fetchFromGitHub {
     owner = "haderech";
     repo = "blanc";
