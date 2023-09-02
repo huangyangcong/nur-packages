96f5505c08a94e8dc17ea4ec144a9ede1d6c623f
YangCongHuang <huangyangcong@gmail.com>
2023-09-02 10:12:25 +0800

    fixed blanc

--- pkgs/blanc/blanc_16.nix
+++ pkgs/blanc/blanc_16.nix
@@ -1,11 +1,11 @@
-{ gcc9Stdenv
+{ gcc8Stdenv
 , fetchFromGitHub
 , pkgs
 , lib
 , llvmPackages
 ,
 }:
-gcc9Stdenv.mkDerivation rec {
+gcc8Stdenv.mkDerivation rec {
   name = "blanc";
   version = "16.0.0";
   buildInputs = with pkgs; [
