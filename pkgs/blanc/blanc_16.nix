0df445ead1c0149d5e2c5eac7c655674ed1fd7da
YangCongHuang <huangyangcong@gmail.com>
2023-09-02 08:49:18 +0800

    fixed blanc

--- pkgs/blanc/blanc_16.nix
+++ pkgs/blanc/blanc_16.nix
@@ -12,9 +12,20 @@ gcc9Stdenv.mkDerivation rec {
     llvmPackages_16.clang-unwrapped
     llvmPackages_16.llvm
     llvmPackages_16.lld
-    icu4c
+    libxml2.dev
   ];
   nativeBuildInputs = with pkgs; [ pkgconfig cmake git python3 ];
+
+  cmakeFlags = [
+    "-DLLVM_BUILD_EXTERNAL_COMPILER_RT=ON"
+    "-DLLVM_BUILD_LLVM_DYLIB=ON"
+    "-DLLVM_ENABLE_LIBCXX=ON"
+    "-DLLVM_ENABLE_RTTI=ON"
+    "-DLLVM_INCLUDE_DOCS=OFF"
+    "-DLLVM_OPTIMIZED_TABLEGEN=ON"
+    "-DLLVM_TARGETS_TO_BUILD=all"
+    "-DCMAKE_BUILD_TYPE=Release"
+  ];
   src = fetchFromGitHub {
     owner = "haderech";
     repo = "blanc";
