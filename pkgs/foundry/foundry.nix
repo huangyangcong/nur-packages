ee52bc48582eb2eef4bf5b6277bf7031f01cce36
YangCongHuang <huangyangcong@gmail.com>
2023-09-23 17:55:44 +0800

    add foundry

--- pkgs/foundry/foundry.nix
+++ pkgs/foundry/foundry.nix
@@ -7,6 +7,7 @@ rustPlatform.buildRustPackage rec {
     owner = "foundry-rs";
     repo = "foundry";
     rev = "4665d7ce4b3b572163cc04b33b4fd190e28f2c5f";
+    sha256 = "sha256-yZpTj3MlWYDw3Se7ZwwvNnqB4XpdPVqHWY2hK18KIIU=";
   };
   cargoLock = {
     lockFile = "${src}/Cargo.lock";
