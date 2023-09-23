417f319fd2400ab47adb1e9356a33d586a33749b
YangCongHuang <huangyangcong@gmail.com>
2023-09-23 14:44:22 +0800

    add foundry

--- pkgs/foundry/foundry.nix
+++ pkgs/foundry/foundry.nix
@@ -1,4 +1,4 @@
-{ pkgs, rustPlatform, fetchFromGitHub }:
+{ lib, pkgs, rustPlatform, fetchFromGitHub }:
 rustPlatform.mkDerivation rec {
   pname = "foundry";
   version = "nightly-4665d7ce4b3b572163cc04b33b4fd190e28f2c5f";
