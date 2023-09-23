b1c44fc709d4fd7a52c624c7332e125209de6815
YangCongHuang <huangyangcong@gmail.com>
2023-09-23 16:19:43 +0800

    add foundry

--- pkgs/foundry/foundry.nix
+++ pkgs/foundry/foundry.nix
@@ -9,7 +9,11 @@ rustPlatform.buildRustPackage rec {
     rev = "4665d7ce4b3b572163cc04b33b4fd190e28f2c5f";
     sha256 = "sha256-yZpTj3MlWYDw3Se7ZwwvNnqB4XpdPVqHWY2hK18KIIU=";
   };
-
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    # Allow dependencies to be fetched from git and avoid having to set the outputHashes manually
+    allowBuiltinFetchGit = true;
+  };
   installPhase = ''
     cargo install --path ./crates/forge --profile local --force --locked
     cargo install --path ./crates/cast --profile local --force --locked
