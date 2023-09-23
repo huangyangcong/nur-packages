341f7dc4fb2b2cffe5dbde2d12d49d95d741f162
YangCongHuang <huangyangcong@gmail.com>
2023-09-23 17:53:35 +0800

    add foundry

--- pkgs/foundry/foundry.nix
+++ pkgs/foundry/foundry.nix
@@ -34,7 +34,7 @@ rustPlatform.buildRustPackage rec {
     cargo install --path ./crates/chisel --profile local --force --locked
   '';
 
-  cargoSha256 = "sha256-IfuEqo5z+K+XbDSFgDIycpOLiBeB9iCz/sj8i+lB8dw=";
+  cargoSha256 = lib.fakeHash;
   meta = with lib; {
     description = "Foundry is a blazing fast, portable and modular toolkit for Ethereum application development";
     homepage = "https://getfoundry.sh/";
